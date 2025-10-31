a8ec1e7fb0514cded923c11ae13f0c2b915b04c1
17Fidele <fzuobai.2302849@stu.cu.edu.ng>
2025-10-31 16:55:12 +0100

    Update requirements.txt with full dependencies

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the model
+# Load the trained model
 model = load_model('face_emotionModel.h5')
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-# Route for homepage
+# Homepage route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = ''
@@ -19,19 +19,22 @@ def index():
         if 'file' in request.files:
             file = request.files['file']
             if file.filename != '':
-                img = Image.open(file).convert('L').resize((48,48))
-                img_array = img_to_array(img)/255.0
+                # Open image, convert to grayscale and resize
+                img = Image.open(file).convert('L').resize((48, 48))
+                img_array = img_to_array(img) / 255.0
                 img_array = np.expand_dims(img_array, axis=0)
 
+                # Make prediction
                 prediction = model.predict(img_array)
                 emotion = emotion_labels[np.argmax(prediction)]
                 result = f'Predicted Emotion: {emotion}'
 
-                # Save output to database.txt
+                # Save to database.txt
                 with open('database.txt', 'a') as db:
                     db.write(f'User uploaded: {file.filename}, Prediction: {emotion}\n')
 
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # For Render deployment, use host='0.0.0.0'
+    app.run(host='0.0.0.0', port=5000, debug=True)
